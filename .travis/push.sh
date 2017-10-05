37f6327431eea33a99d91a3d3ecf4945578be366
oookli <oklika@gmail.com>
2017-10-05 19:53:07 +0300

    add travis CI to deploy gh-pages

---
+++ .travis/push.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gh-pages
+  rm -rf `ls -A -1  | grep -v dist`
+  ls dist/* .
+  rm -r dist
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/AUMLviv/Mykolay-web-site.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages
+}
+
+setup_git
+commit_website_files
+upload_files
